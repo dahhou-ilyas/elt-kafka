48c2563a4a38c1687edbcea17fddc4b6d3cc9050
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-09-12 12:15:46 +0100

    sume moduf

--- elt-scripts/consumer-kafka.py
+++ elt-scripts/consumer-kafka.py
@@ -1,17 +1,51 @@
 from kafka import KafkaConsumer, TopicPartition
+import json
+import re
+import sys
 
-import pandas as pd
+
+def clean_json_string(raw_value):
+    # Retirer les retours à la ligne et les espaces inutiles
+    json_str = re.sub(r"[\r\n]+", " ", raw_value)
+
+    # Remplacer les apostrophes non échappées par des guillemets
+    json_str = re.sub(r"(?<!\\)'", '"', json_str)
+
+    # Remplacer les occurrences de 'nan' et 'None' par 'null'
+    json_str = re.sub(r"nan", "null", json_str, flags=re.IGNORECASE)
+    json_str = re.sub(r"None", "null", json_str)
+
+    return json_str
+
 
 consumer = KafkaConsumer(
-    bootstrap_servers='localhost:9092',  # Important pour démarrer du début si l'offset n'est pas défini
-    enable_auto_commit=False  # Désactiver l'auto-commit pour contrôler manuellement l'offset
+    bootstrap_servers='localhost:9092',
+    group_id='my-consumer-group',
+    enable_auto_commit=False,
+    auto_offset_reset='earliest'
 )
 
 partition = TopicPartition('sales_raw_data', 0)
-
 consumer.assign([partition])
+data=[]
+try:
+    for msg in consumer:
+        try:
+            raw_value = msg.value.decode('utf-8')
+            json_str = clean_json_string(raw_value)
+            dict_from_value = json.loads(json_str)
+            data.append(dict_from_value)
+            consumer.commit()
+
+        except (ValueError, SyntaxError) as e:
+            print(f"Erreur lors de la conversion en dictionnaire : {e}")
+        except Exception as e:
+            print(f"Erreur lors du traitement du message : {e}")
 
 
-
-for msg in consumer:
-    print(f"Offset: {msg.offset}, Key: {msg.key}, Value: {msg.value.decode('utf-8')}")
+except KeyboardInterrupt:
+    print("Arrêt du consommateur.")
+finally:
+    # Nettoyage et fermeture du consommateur
+    consumer.close()
+    print("Consommateur fermé proprement.")
--- elt-scripts/producer-kafka.py
+++ elt-scripts/producer-kafka.py
@@ -1,7 +1,6 @@
-import pandas as pd
 from kafka import KafkaProducer
 import os
-
+import pandas as pd
 KAFKA_BOOTSTRAP_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 KAFKA_API_VERSION = os.environ.get("KAFKA_API_VERSION", "7.4.1")
 
@@ -10,12 +9,22 @@ producer = KafkaProducer(
     api_version=KAFKA_API_VERSION
 )
 
+def on_send_success(record_metadata):
+    print(f"Message envoyé à {record_metadata.topic} partition {record_metadata.partition} offset {record_metadata.offset}")
 
-def read_csv_and_send_to_kafka(file_path,topic):
-    df = pd.read_csv(file_path,encoding='ISO-8859-1')
-    for index,row in df.iterrows():
-        message=str(row.to_dict()).encode('utf-8')
-        producer.send(topic, message)
+def on_send_error(excp):
+    print(f"Erreur lors de l'envoi du message: {excp}")
 
+def read_csv_and_send_to_kafka(file_path, topic):
+    df = pd.read_csv(file_path, encoding='ISO-8859-1')
+    i = 0
+    for index, row in df.iterrows():
+        message = str(row.to_dict()).encode('utf-8')
+        print(f"Envoi du message: {message}")
+        producer.send(topic, message).add_callback(on_send_success).add_errback(on_send_error)
+        if i > 2:
+            break
+        i += 1
+    producer.flush()
 
-read_csv_and_send_to_kafka("source-data/sales_data_sample.csv","sales_raw_data")
+read_csv_and_send_to_kafka("source-data/sales_data_sample.csv", "sales_raw_data")
