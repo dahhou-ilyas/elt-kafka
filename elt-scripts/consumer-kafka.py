137e7e219f3f8a3c479bb8a34d9ef6c795ea1dca
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-09-12 12:54:55 +0100

    filter

--- elt-scripts/consumer-kafka.py
+++ elt-scripts/consumer-kafka.py
@@ -1,51 +1,46 @@
 from kafka import KafkaConsumer, TopicPartition
 import json
 import re
-import sys
-
+import pandas as pd
 
 def clean_json_string(raw_value):
-    # Retirer les retours à la ligne et les espaces inutiles
-    json_str = re.sub(r"[\r\n]+", " ", raw_value)
+    json_str = re.sub(r"[\r\n]+", "", raw_value)
 
-    # Remplacer les apostrophes non échappées par des guillemets
     json_str = re.sub(r"(?<!\\)'", '"', json_str)
 
-    # Remplacer les occurrences de 'nan' et 'None' par 'null'
     json_str = re.sub(r"nan", "null", json_str, flags=re.IGNORECASE)
     json_str = re.sub(r"None", "null", json_str)
 
     return json_str
 
-
+# Création du consommateur Kafka
 consumer = KafkaConsumer(
     bootstrap_servers='localhost:9092',
-    group_id='my-consumer-group',
+    group_id='my-consumer-group',  # Assurez-vous d'utiliser un group_id unique si nécessaire
     enable_auto_commit=False,
-    auto_offset_reset='earliest'
+    auto_offset_reset='latest'
 )
 
+# Assignation de la partition
 partition = TopicPartition('sales_raw_data', 0)
 consumer.assign([partition])
-data=[]
+
+data = []
+
 try:
     for msg in consumer:
         try:
             raw_value = msg.value.decode('utf-8')
             json_str = clean_json_string(raw_value)
             dict_from_value = json.loads(json_str)
-            data.append(dict_from_value)
-            consumer.commit()
-
+            df = pd.DataFrame([dict_from_value])
+            df_cleaned = df.dropna(subset=['TERRITORY'])
+            print(df_cleaned)
         except (ValueError, SyntaxError) as e:
             print(f"Erreur lors de la conversion en dictionnaire : {e}")
         except Exception as e:
             print(f"Erreur lors du traitement du message : {e}")
 
-
-except KeyboardInterrupt:
-    print("Arrêt du consommateur.")
 finally:
-    # Nettoyage et fermeture du consommateur
     consumer.close()
     print("Consommateur fermé proprement.")
